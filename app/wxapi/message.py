8a5cd659fe000fe5accaaec391b2730c12e78f8c
newtorn <codertorn@gmail.com>
2019-03-15 15:53:54 +0800

    version1.0.0 finished

--- app/wxapi/message.py
+++ app/wxapi/message.py
@@ -453,7 +453,6 @@ class MessageProcessor(object):
         event = self.xml_rec.find('Event').text
         if event == 'subscribe':
             reply = self.text_process('关注')
-            return self.mb.build_text_msg(self.from_user, self.to_user, self.create_time, self.msg_id, reply)
         elif event == 'unsubscribe':
             reply = 'success'
             user = User.query.filter(User.openid == g.openid).first()
@@ -466,7 +465,7 @@ class MessageProcessor(object):
             reply = self.text_process('图书信息')
         elif event == 'event3':
             reply = '<a href="{app_domain}public/book?openid={openid}&book_name={book_name}">好好学习，天天向上，HELLO~我是河小博~点击查看图书详情</a>'.format(
-                app_domain = app_config.APP_DOMAIN，
+                app_domain = app_config.APP_DOMAIN,
                 openid = g.openid,
                 book_name = grp[1])
         elif event == 'event4':
@@ -480,7 +479,7 @@ class MessageProcessor(object):
             reply = self.text_process('后勤报修')
         elif event == 'event7':
             reply = self.text_process('河大全景')
-        return  self.mb.build_text_msg(self.from_user, self.to_user, self.create_time, self.msg_id, reply)
+        return self.mb.build_text_msg(self.from_user, self.to_user, self.create_time, self.msg_id, reply)
 
     def image_reply(self):
         """处理图片内容回复"""
