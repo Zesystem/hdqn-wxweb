7ae64c8737e65b29aa2f1a2baa7e2cd7a2659675
newtorn <codertorn@gmail.com>
2019-03-17 14:21:05 +0800

    version1.0.0 finished

--- manager.py
+++ manager.py
@@ -7,12 +7,13 @@
 #
 ###########################################
 
-from app import app, db, models
+from app import app as application
+from app import db, models
 from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 
-manager = Manager(app)
-migrate = Migrate(app, db)
+manager = Manager(application)
+migrate = Migrate(application, db)
 
 manager.add_command('runserver', Server(host='127.0.0.1', port=8000, use_debugger=True))
 manager.add_command('db', MigrateCommand)
