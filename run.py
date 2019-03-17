810b76701c62f9ffa73faffdbb92a37bb84df0ba
newtorn <codertorn@gmail.com>
2019-03-17 13:58:46 +0800

    version1.0.0 finished

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from app import app
+from app import app as application
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8000)
+    application.run(debug=True, host='127.0.0.1', port=8000)
