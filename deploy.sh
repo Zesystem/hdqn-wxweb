a7ce95f4dacbcadaab3648f5795611f763deed00
newtorn <codertorn@gmail.com>
2019-03-17 16:46:43 +0800

    version1.0.0 finished

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo 'Starting to deploy hdqnwxweb service ...'
+BASEPATH=$(cd `dirname $0`; pwd)
+echo '==>Deploying from path: $BASHPATH ...'
+echo '==>Copying file: $BASEPATH/conf/default.conf to: /etc/nginx/conf.d/default.conf ...'
+cp $BASEPATH/conf/default.conf /etc/nginx/conf.d/default.conf
+echo '==>Copying file: $BASEPATH/conf/default.conf to: /etc/nginx/conf.d/default.conf ...'
+cp $BASEPATH/conf/hdqnwxweb.conf /etc/init.d/hdqnwxweb
+echo 'Modifying the authority of deploy files ...'
+chmod +x /etc/init.d/hdqnwxweb
+chmod +x /etc/nginx/conf.d/default.conf
+echo 'Add service to system ...'
+chkconfig --add hdqnwxweb
+chkconfig --level 345 hdqnwxweb on
+chkconfig --list hdqnwxweb
+echo 'Successfully deploy hdqnwxweb!'
+echo 'Use \`service hdqnwxweb start\` to start service.'
