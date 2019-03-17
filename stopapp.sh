132bf6c880bee2f3fedb2ad5a8fd7ab5f7163221
newtorn <codertorn@gmail.com>
2019-03-17 16:59:20 +0800

    version1.0.0 finished

--- stopapp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-NAME="app_wechat"
-FLASKDIR=/root/hdqn-wxweb
-UWSGIPATH=/root/.pyenv/shims/uwsgi
-UWSGICONF=/root/hdqn-wxweb/uwsgi.ini
-
-echo "stopping $NAME as `whoami`"
-killall -9 uwsgi
-echo "success to stop $NAME application" 
