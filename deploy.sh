6f07bede660440a6e660a070055038b8260d42da
newtorn <codertorn@gmail.com>
2019-03-17 16:49:16 +0800

    version1.0.0 finished

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ echo '==>Deploying from path: $BASHPATH ...'
 echo '==>Copying file: $BASEPATH/conf/default.conf to: /etc/nginx/conf.d/default.conf ...'
 cp $BASEPATH/conf/default.conf /etc/nginx/conf.d/default.conf
 echo '==>Copying file: $BASEPATH/conf/hdqnwxwe to: /etc/init.d/hdqnwxweb ...'
-cp $BASEPATH/conf/hdqnwxwe /etc/init.d/hdqnwxweb
+cp $BASEPATH/conf/hdqnwxweb /etc/init.d/hdqnwxweb
 echo 'Modifying the authority of deploy files ...'
 chmod +x /etc/init.d/hdqnwxweb
 chmod +x /etc/nginx/conf.d/default.conf
