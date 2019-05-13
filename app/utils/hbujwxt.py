d45eacccba590efbed469cd24b2d2600f134b71e
newtorn <codertorn@gmail.com>
2019-05-14 05:33:50 +0800

    fixed evaluate

--- app/utils/hbujwxt.py
+++ app/utils/hbujwxt.py
@@ -343,16 +343,15 @@ class HbuJwxt(object):
     def evaluation_post(self, data):
         '''教学评估提交
         '''
-        # try:
-        return {'code' : status.CODE_SUCCESS}
-        data = urllib.parse.urlencode(data, encoding='gb2312')
-        url = 'http://{ip}/jxpgXsAction.do?oper=wjpg'.format(ip=self.ip)
-        rep = self.session.request('POST', url, data, headers=self.headers)
-        if '评估成功' in rep.content.decode('GBK'):
-            return {'code' : status.CODE_SUCCESS}
-        return {'code' : status.CODE_FAILED}
-        # except:
-            # return {'code': status.CODE_FAILED}
+        try:
+            data = urllib.parse.urlencode(data, encoding='gb2312')
+            url = 'http://{ip}/jxpgXsAction.do?oper=wjpg'.format(ip=self.ip)
+            rep = self.session.request('POST', url, data, headers=self.headers)
+            if '评估成功' in rep.content.decode('GBK'):
+                return {'code' : status.CODE_SUCCESS}
+            return {'code' : status.CODE_FAILED}
+        except:
+            return {'code': status.CODE_FAILED}
 
 if __name__ == '__main__':
     pass
