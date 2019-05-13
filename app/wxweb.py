22aacd4cbdc8aaee109d3bd81233cfc72713fea8
newtorn <codertorn@gmail.com>
2019-05-14 05:08:46 +0800

    fixed evaluate

--- app/wxweb.py
+++ app/wxweb.py
@@ -138,7 +138,7 @@ def evaluate():
                     if data[key] is None:
                         return "<script>alert('请填写完整数据！');window.history.back();</script>"
                 res = hbujwxt.evaluation_post(data)
-                with open('~/err.log', 'w') as f:
+                with open('/root/err.log', 'w') as f:
                     f.write(str(res))
                 if res['code'] == status.CODE_SUCCESS:
                     return "<script>alert('评教成功！');window.location.href='/wxweb/evaluate';</script>"
