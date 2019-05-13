a475fbfde87c2d287bb79d4165691b76fcfd61a7
newtorn <codertorn@gmail.com>
2019-05-14 06:05:48 +0800

    fixed evaluate

--- app/utils/hbujwxt.py
+++ app/utils/hbujwxt.py
@@ -344,9 +344,9 @@ class HbuJwxt(object):
         '''教学评估提交
         '''
         try:
+            with open('/root/err.log', 'w') as f:
+                f.wrtie('hello')
             if not self.jw_login(userinfo):
-                with open('/root/err.log', 'w') as f:
-                    f.wrtie('hello')
                 return {'code': status.CODE_FAILED}
             data = urllib.parse.urlencode(data, encoding='gb2312')
             url = 'http://{ip}/jxpgXsAction.do?oper=wjpg'.format(ip=self.ip)
