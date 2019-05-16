65b69ddab179ba6846dc2333b603c6b6c4f1716e
newtorn <codertorn@gmail.com>
2019-05-16 10:37:16 +0800

    add nginx control

--- bin/hdqnwxweb.sh
+++ bin/hdqnwxweb.sh
@@ -8,6 +8,7 @@ if [ ! -n "$1" ];then
 fi
 
 if [ $1 = start ];then
+    service nginx start
     psid=`ps aux | grep "uwsgi" | grep -v "grep" | wc -l`        
     if [ $psid -gt 4 ];then
         echo "uwsgi is running!"
@@ -18,8 +19,10 @@ if [ $1 = start ];then
     fi
 elif [ $1 = stop ];then
     killall -9 uwsgi
+    service nginx stop
     echo "Stop uwsgi service [OK]"
 elif [ $1 = restart ];then
+    service nginx restart
     killall -9 uwsgi
     /root/.pyenv/shims/uwsgi /root/hdqn-wxweb/conf/uwsgi.ini
     echo "Restart uwsgi service [OK]"
