8e5261009dbeebab73039871dedeef6e05a201af
newtorn <codertorn@gmail.com>
2019-05-16 11:06:24 +0800

    database lock

--- app/wxweb.py
+++ app/wxweb.py
@@ -125,7 +125,7 @@ def evaluate():
     courseinfo = hbujwxt.evaluation_get_courses(userinfo)
     if request.method == 'GET':
         if not request.args.get('premsg'):
-            return "<script>alert('%s')</script>" % user
+            return "<script>alert('%r')</script>" % userinfo
             # return render('/wxweb/Evaluate/index.html', courseinfo=courseinfo)
         else:
             try:
