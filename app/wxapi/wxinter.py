e6fc0d3242cea62707ba232d05d284984322b4d2
newtorn <codertorn@gmail.com>
2019-05-17 14:47:41 +0800

    update

--- app/wxapi/wxinter.py
+++ app/wxapi/wxinter.py
@@ -14,28 +14,30 @@ from flask import request, make_response, render_template
 
 def wx_check():
     """微信token接口验证"""
-    if request.method == 'GET':
-        try:
-            data = request.args
-            if len(data) == 0:
-                return render_template('public/wxinter.html')
-            signature = data.get('signature')
-            timestamp = data.get('timestamp')
-            nonce = data.get('nonce')
-            echostr = data.get('echostr')
-            token = 'dev4hdqn'
+    data = request.args
+    if len(data) == 0:
+        return render_template('public/wxinter.html')
+    signature = data.get('signature')
+    timestamp = data.get('timestamp')
+    nonce = data.get('nonce')
+    echostr = data.get('echostr')
+    token = 'dev4hdqn'
 
-            grp = [token, timestamp, nonce]
-            grp.sort()
-            hashcode = hashlib.sha1(''.join(grp).encode('utf-8')).hexdigest()
+    grp = [token, timestamp, nonce]
+    grp.sort()
+    hashcode = hashlib.sha1(''.join(grp).encode('utf-8')).hexdigest()
 
-            print("handle/GET func: hashcode, signature:", hashcode, signature)
-            if hashcode == signature:
-                return make_response(echostr)
-            return render_template('public/wxinter.html')
-        except:
-            return render_template('public/404.html')
+    # print("handle/GET func: hashcode, signature:", hashcode, signature)
+    if hashcode != signature:
+        return render_template('public/wxinter.html')
     else:
-        rec = request.stream.read()
-        rep = mp.check_reply(rec)
-        return rep
+        if request.method == 'GET':
+            if not echostr:
+                return render_template('public/wxinter.html')
+            return make_response(echostr)
+        elif request.method == 'POST':
+            rec = request.stream.read()
+            if not rec:
+                return render_template('public/wxinter.html')
+            rep = mp.check_reply(rec)
+            return rep
